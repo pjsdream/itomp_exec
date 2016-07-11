1fd5819c104bc78890f1c9e2607bc696269b49e1
jaesungp <jaesungp@cs.unc.edu>
2016-07-11 16:47:50 -0400

    writing time cost

--- itomp_exec/src/cost/time_cost.cpp
+++ itomp_exec/src/cost/time_cost.cpp
@@ -17,6 +17,31 @@ void TimeCost::addCost()
 
     double& cost = optimizer.cost();
 
+    const int num_interpolated_variables = optimizer.getNumInterpolatedConfigurations();
+    const int num_robot_joints = optimizer.getNumRobotJoints();
+
+    for (int i=0; i<num_interpolated_variables; i++)
+    {
+        const double interpolated_time = optimizer.getInterpolatedTime(i);
+
+        for (int j=0; j<num_robot_joints; j++)
+        {
+            const ITOMPOptimizer::GoalLinkPose& goal_link_pose = optimizer.getGoalLinkPose(i);
+
+            const double& position_weight = goal_link_pose.position_weight;
+            const double& orientation_weight = goal_link_pose.orientation_weight;
+
+            if (position_weight != 0.)
+            {
+                const Eigen::Vector3d& link_transform = optimizer.getInterpolatedLinkTransform(i, j).translation();
+            }
+
+            if (orientation_weight != 0.)
+            {
+                const Eigen::Quaterniond& link_orientation( optimizer.getInterpolatedLinkTransform(i, j).linear() );
+            }
+        }
+    }
 }
 
 void TimeCost::addDerivative()
