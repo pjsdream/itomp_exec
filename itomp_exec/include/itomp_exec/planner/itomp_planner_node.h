309ccc5829701ccfe6ad2b09410c149cfd922444
jaesungp <jaesungp@cs.unc.edu>
2016-10-30 15:15:55 -0400

    removed old codes

--- itomp_exec/include/itomp_exec/planner/itomp_planner_node.h
+++ itomp_exec/include/itomp_exec/planner/itomp_planner_node.h
@@ -24,172 +24,30 @@
 namespace itomp_exec
 {
 
-// planning_interface::PlannerManager is not appropriate for the purpose of replanning.
-// ITOMP provides 
-class ITOMPPlannerNode
+// The MoveIt class "planning_interface::PlannerManager" is not appropriate for the purpose of replanning.
+// Thus, a new class for replanning is defined.
+class ItompPlanner
 {
 public:
 
-    enum ITOMPAlgorithm
-    {
-        ITOMPFixedTrajectoryDuration = 0,
-        ITOMPFlexibleTrajectoryDuration,
-    };
+    ItompPlanner(robot_model::RobotModelConstPtr robot_model, const ros::NodeHandle& node_handle = ros::NodeHandle("~"));
+    ItompPlanner(const ros::NodeHandle& node_handle = ros::NodeHandle("~"));
 
-    struct GoalPositionConstraint
-    {
-        std::string link_name;
-        Eigen::Vector3d position;
-        double weight;
+    void setTimestep(double timestep);
+    void setTrajectoryDuration(double duration);
 
-        GoalPositionConstraint(){}
-        GoalPositionConstraint(const std::string link_name, const Eigen::Vector3d& position, double weight = 1.)
-            : link_name(link_name), position(position), weight(weight)
-        {}
-    };
+    void planForOneTimestep();
 
-    struct GoalOrientationConstraint
-    {
-        std::string link_name;
-        Eigen::Quaterniond orientation;
-        double weight;
-
-        GoalOrientationConstraint(){}
-        GoalOrientationConstraint(const std::string link_name, const Eigen::Quaterniond& orientation, double weight = 1.)
-            : link_name(link_name), orientation(orientation), weight(weight)
-        {}
-    };
-
-    struct ITOMPPlannerOptions
-    {
-        int num_trajectories;
-        double trajectory_duration;
-        int num_milestones;
-        int num_interpolation_samples;
-        double planning_timestep;
-        double goal_tolerance;
-
-        ITOMPAlgorithm itomp_algorithm;
-        
-        std::vector<std::pair<std::string, double> > cost_weights;
-        std::vector<std::string> collision_sphere_link_names;
-
-        // ITOMP dynamic obstacle conservative bound
-        struct
-        {
-            double dynamic_obstacle_max_speed;
-            double dynamic_obstacle_duration;
-        } conservative_algorithm;
-    };
-    
-public:
-
-    ITOMPPlannerNode(robot_model::RobotModelConstPtr robot_model, const ros::NodeHandle& node_handle = ros::NodeHandle("~"));
-    ITOMPPlannerNode(const ros::NodeHandle& node_handle = ros::NodeHandle("~"));
-
-    inline const PlanningScene& getPlanningScene() const
-    {
-        return planning_scene_;
-    }
-
-    inline robot_model::RobotModelConstPtr getMoveitRobotModel() const
-    {
-        return moveit_robot_model_;
-    }
-
-    inline BoundingSphereRobotModelPtr getRobotModel() const
-    {
-        return robot_model_;
-    }
-
-    inline double getTrajectoryDuration() const
-    {
-        return options_.trajectory_duration;
-    }
-    
-    inline double getPlanningTimestep() const
-    {
-        return options_.planning_timestep;
-    }
-    
-    inline void setTrajectoryDuration(double trajectory_duration)
-    {
-        options_.trajectory_duration = trajectory_duration;
-    }
-    
-    inline void setPlanningTimestep(double planning_timestep)
-    {
-        options_.planning_timestep = planning_timestep;
-    }
-
-    void setRobotModel(const robot_model::RobotModelConstPtr& robot_model);
-
-    void addStaticObstacle(const std::string& mesh_filename, const Eigen::Affine3d& transformation = Eigen::Affine3d::Identity());
-    void addStaticObstacles(const std::vector<std::string>& mesh_filename, const std::vector<Eigen::Affine3d>& transformation);
-    
-    void setMotionPlanRequest(const planning_interface::MotionPlanRequest& req);
-
-    bool planAndExecute(planning_interface::MotionPlanResponse& res);
-    
-    // DEBUG
-    void printParams();
-    void printControllers();
-    void printCostWeights();
 
 private:
-    
-    // planning algorithms
-    void initializeOptimizers();
-    bool planAndExecuteFixedTrajectoryDuration();
-    bool planAndExecuteFlexibleTrajectoryDuration();
 
-    // initialization functions
-    void initialize();
-    void loadParams();
-    void loadStaticObstacles();
-
-    void optimizeAllTrajectories();
-
-    // visualization
-    void visualizePlanningScene();
-    void visualizeGoalConstraints();
-
-    Eigen::Affine3d getRobotRootTransform();
-
-    tf::TransformListener transform_listener_;
-    
-    // ros & moveit stuffs
     ros::NodeHandle node_handle_;
-    robot_model::RobotModelConstPtr moveit_robot_model_;
-    trajectory_execution_manager::TrajectoryExecutionManagerPtr trajectory_execution_manager_;
 
-    BoundingSphereRobotModelPtr robot_model_;
-    RobotState start_state_;
+    RobotModel robot_model_;
 
-    // multithreaded optimizers
-    std::vector<ITOMPOptimizer> optimizers_;
-    std::vector<Thread*> optimizer_threads_;
-    
-    std::vector<GoalPositionConstraint> goal_link_positions_;
-    std::vector<GoalOrientationConstraint> goal_link_orientations_;
-    
-    // planning environment
-    std::string planning_group_name_;
-    PlanningScene planning_scene_; //!< spheres should be added to optimizer independantly
-
-    // ITOMP options
-    ITOMPPlannerOptions options_;
-
-    // virtual human
-    ros::Publisher virtual_human_arm_request_publisher_;
-
-    // visualization marker array publisher for planning scene
-    ros::Publisher planning_scene_visualization_publisher_;
-    ros::Publisher goal_constraint_visualization_publisher_;
+    double timestep_;
+    double trajectory_duration_;
 };
-
-typedef std::shared_ptr<ITOMPPlannerNode> ITOMPPlannerNodePtr;
-typedef std::shared_ptr<const ITOMPPlannerNode> ITOMPPlannerNodeConstPtr;
 
 }
 
